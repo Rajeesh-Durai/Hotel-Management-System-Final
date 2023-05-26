0d0331de4690c6983f5383971f8cd0c4fd713a4d
Rajeesh-Durai <rajeesh.kanini@gmail.com>
2023-05-26 11:25:21 +0530

    first commit

---
+++ Auth/Register.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBooking.Models
+{
+    public class Register
+    {
+        [Key]
+        public int RegId { get; set; }
+        public string? Name { get; set; }
+
+        public string EmailId { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Roles { get; set; } = string.Empty;
+    }
+}
